1d3d3e353e3527b2afa2f1276ab30b29b21130b8
elninosantz <tiaugustocesar@gmail.com>
2026-03-23 22:18:56 -0300

    feat: update index.html

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from src.load.save_csv_slicing import save_csv_nome_id, save_csv_id_uf, save_csv_id_partido
 from src.load.save_csv_valor_total_despesa import save_csv_os_mais_gastos_filtrados
+from src.transform.cleaning_legislaturas import get_data_legislaturas
+import webbrowser
 
 import polars as pl
 
+def legislatura():
+    base_temporal_legislatura = get_data_legislaturas
+    texto_encoded = urllib.parse.quote(base_temporal_legislatura)
+    url = f"https://elninosantz.github.io/deputado_analysis/index.html?legislatura={texto_encoded}"
+    webbrowser.open(url)
+
 if __name__ == "__main__":
-    save_csv_os_mais_gastos_filtrados()
+    legislatura()
